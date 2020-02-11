86b6461a253bbfe796a0a47245d35d28749c32af
Gunnar Unger <gunnar.unger@gmail.com>
2020-02-12 08:15:04 +1100

    update

--- module-list.js
+++ module-list.js
@@ -4,8 +4,12 @@
         "panel-main":    			{url:"$H/m/record-list.html",router:1},
         "comeback-email-address-data":     {url:"$H/m/comeback-email-address-data.html",Table:"comeback-email-address",form_module:"comeback-email-address-form",private:1},
         "comeback-email-address-form":     {url:"$H/m/comeback-email-address-form.html",Table:"comeback-email-address",private:1},
-        "comeback-item-list-data":     {url:"$H/m/comeback-item-list-data.html",Table:"comeback-item-list",form_module:"comeback-item-list-form",private:1},
-        "comeback-item-list-form":     {url:"$H/m/comeback-item-list-form.html",Table:"comeback-item-list",private:1},
+        "comeback-charge-list-data":     {url:"$H/m/comeback-charge-list-data.html",Table:"comeback-charge-list",form_module:"comeback-charge-list-form",private:1},
+        "comeback-charge-list-form":     {url:"$H/m/comeback-charge-list-form.html",Table:"comeback-charge-list",private:1},
+        "comeback-order-data":     {url:"$H/m/comeback-order-data.html",Table:"comeback-order",form_module:"comeback-order-form",private:1},
+        "comeback-order-form":     {url:"$H/m/comeback-order-form.html",Table:"comeback-order",private:1},
+        "comeback-items-data":     {url:"$H/m/comeback-items-data.html",Table:"comeback-items",form_module:"comeback-items-form",private:1},
+        "comeback-items-form":     {url:"$H/m/comeback-items-form.html",Table:"comeback-items",private:1},
     }
     for(p in modules){
         $vm.module_list[p]=modules[p]; 
